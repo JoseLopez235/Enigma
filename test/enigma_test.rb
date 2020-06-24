6e83e8e86c3dadfb5c925a298495f15b08e168a1
Jose Lopez <joselopez11394@gmail.com>
2020-06-24 13:19:32 -0400

    created the alphabet

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,4 +7,8 @@ describe Enigma do
   it "should exist" do
     expect(enigma).to be_kind_of(Enigma)
   end
+
+  it "should be the whole alphabet with a space at the end" do
+    expect(enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
 end
